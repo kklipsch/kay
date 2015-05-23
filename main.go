c18f689f5c11c8b2c6fb44f25c58f0f47143bfaf
Kasey Klipsch <junk@klipsch.net>
2015-05-23 08:23:07 -0500

    remove unneeded commands

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/kklipsch/cli"
 	"github.com/kklipsch/kay/chapter"
@@ -28,33 +27,6 @@ func newKay() *cli.App {
 	app.EnableBashCompletion = true
 	app.Commands = []cli.Command{
 		{
-			Name:   "years",
-			Usage:  "List available years",
-			Action: inKay("years"),
-			Flags: []cli.Flag{
-				cli.BoolFlag{
-					Name:  "missing, m",
-					Usage: "show missing years instead of existing ones.",
-				},
-				cli.IntFlag{
-					Name:  "from, f",
-					Usage: "which year to start on (defaults to first year in index if not provided)",
-					Value: 1900,
-				},
-				cli.IntFlag{
-					Name:  "to, t",
-					Usage: "which year to end on (defaults to this year if not provided)",
-					Value: time.Now().Year(),
-				},
-			},
-		},
-		{
-			Name:   "tags",
-			Usage:  "List available tags",
-			Action: inKay("tags"),
-			Flags:  []cli.Flag{},
-		},
-		{
 			Name:   "add",
 			Usage:  "[files] - add files to an index.",
 			Action: kayBased(commands.Add),
@@ -68,24 +40,6 @@ func newKay() *cli.App {
 					Usage: "a csv list of tags for the file. Will NOT fail if not provided or parsed.",
 				},
 			},
-		},
-		{
-			Name:   "rm",
-			Usage:  "[files] - rm files from the index.",
-			Action: inKay("rm"),
-			Flags:  []cli.Flag{},
-		},
-		{
-			Name:   "html",
-			Usage:  "create the website",
-			Action: inKay("html"),
-			Flags:  []cli.Flag{},
-		},
-		{
-			Name:   "txt",
-			Usage:  "[files] - show the text (if available) for a given file",
-			Action: inKay("txt"),
-			Flags:  []cli.Flag{},
 		},
 		{
 			Name:   "stat",
